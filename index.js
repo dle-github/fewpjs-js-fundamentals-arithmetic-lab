0e329532c6486a0973fd547c547c7a4b8c398b55
derek <derekle.creative@gmail.com>
2021-08-10 17:59:02 -0500

    commit tree 1ccd9102ee3a086c8050d7ecf5a84066aa147b02

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+const mod = num3 / num4;
+
+const max = Math.max(20)
